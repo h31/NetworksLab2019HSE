583bde510181628b9d04a00777b3e3fa8457e677
karvozavr <karvozavr@gmail.com>
2019-02-04 20:57:17 +0300

    Finish protocol

--- tcp/server/include/response.hpp
+++ tcp/server/include/response.hpp
@@ -116,5 +116,91 @@ struct pstp_confirm_payment_response {
 };
 
 // TODO get_requests_for_payments_response
+struct pstp_get_request_for_payments_response {
 
-// TODO get_payment_results_response
+    explicit pstp_get_request_for_payments_response(
+            uint8_t code,
+            std::vector<std::pair<std::string, uint64_t>> requests = std::vector<std::pair<std::string, uint64_t>>())
+            : header({GET_REQUESTS_FOR_PAYMENTS, code, 0}), requests(std::move(requests)) {}
+
+    uint32_t content_size() {
+        uint32_t size = sizeof(uint32_t);
+        for (auto const &val : requests) {
+            size += (val.first.length() + 1 + std::to_string(val.second).length()) * sizeof(char);
+        }
+        return size;
+    }
+
+    /**
+     * Format:
+     * number_of_requests (uint32_t)
+     * request (wallet_id_cstr\0amount_cstr_decimal\0)
+     */
+    void serialize(uint8_t *buffer) {
+        header.content_size = content_size();
+        *(reinterpret_cast<pstp_response_header *>(buffer)) = header;
+        buffer += sizeof(header);
+        *(reinterpret_cast<uint32_t *>(buffer)) = static_cast<uint32_t>(requests.size());
+        buffer += sizeof(uint32_t);
+        for (auto const &val : requests) {
+            strncpy(reinterpret_cast<char *>(buffer), val.first.c_str(), val.first.length());
+            buffer += val.first.length();
+            *buffer = '\0';
+            ++buffer;
+
+            auto num_str = std::to_string(val.second);
+            strncpy(reinterpret_cast<char *>(buffer), num_str.c_str(), num_str.length());
+            buffer += num_str.length();
+            *buffer = '\0';
+            ++buffer;
+        }
+    }
+
+    pstp_response_header header;
+    std::vector<std::pair<std::string, uint64_t>> requests;
+};
+
+
+struct pstp_get_payment_results_response {
+
+    explicit pstp_get_payment_results_response(
+            uint8_t code,
+            std::vector<std::pair<std::string, uint64_t>> results = std::vector<std::pair<std::string, uint64_t>>())
+            : header({GET_REQUESTS_FOR_PAYMENTS, code, 0}), results(std::move(results)) {}
+
+    uint32_t content_size() {
+        uint32_t size = sizeof(uint32_t);
+        for (auto const &val : results) {
+            size += (val.first.length() + 1 + std::to_string(val.second).length()) * sizeof(char);
+        }
+        return size;
+    }
+
+    /**
+     * Format:
+     * number_of_requests (uint32_t)
+     * request (wallet_id_cstr\0amount_cstr_decimal\0)
+     */
+    void serialize(uint8_t *buffer) {
+        header.content_size = content_size();
+        *(reinterpret_cast<pstp_response_header *>(buffer)) = header;
+        buffer += sizeof(header);
+        *(reinterpret_cast<uint32_t *>(buffer)) = static_cast<uint32_t>(results.size());
+        buffer += sizeof(uint32_t);
+        for (auto const &val : results) {
+            strncpy(reinterpret_cast<char *>(buffer), val.first.c_str(), val.first.length());
+            buffer += val.first.length();
+            *buffer = '\0';
+            ++buffer;
+
+            auto num_str = std::to_string(val.second);
+            strncpy(reinterpret_cast<char *>(buffer), num_str.c_str(), num_str.length());
+            buffer += num_str.length();
+            *buffer = '\0';
+            ++buffer;
+        }
+    }
+
+    pstp_response_header header;
+    std::vector<std::pair<std::string, uint64_t>> results;
+};

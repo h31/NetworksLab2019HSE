c5122f235e030d36f5d0e6a6c81e22e7cb99248f
iisuslik <al.s.fedotov1998@yandex.ru>
2019-02-07 16:05:44 +0300

    adding protocol

---
+++ tcp/server/src/server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by iisuslik on 07.02.19.
+//
+
+#include "server.h"
---
+++ tcp/server/src/server.h
@@ -0,0 +1,21 @@
+//
+// Created by iisuslik on 07.02.19.
+//
+
+#ifndef SERVER_SERVER_H
+#define SERVER_SERVER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#include <string.h>
+
+class Server {
+
+};
+
+#endif //SERVER_SERVER_H

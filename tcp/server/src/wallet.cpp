479a802d6cf1baa1023a2721bf578cf583350676
karvozavr <karvozavr@gmail.com>
2019-02-10 14:52:24 +0300

    .gitignore

---
+++ tcp/server/src/wallet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by karvozavr on 10/02/19.
+//
+
+#include "wallet.hpp"
---
+++ tcp/server/src/wallet.hpp
@@ -0,0 +1,14 @@
+//
+// Created by karvozavr on 10/02/19.
+//
+
+#ifndef SERVER_WALLET_HPP
+#define SERVER_WALLET_HPP
+
+
+class wallet {
+
+};
+
+
+#endif //SERVER_WALLET_HPP

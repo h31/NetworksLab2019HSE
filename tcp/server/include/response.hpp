561de82fe860f02db540100e1cf37272fb6289f2
subject-name-here <jura.paradov@gmail.com>
2019-02-16 14:53:26 +0300

    fixed response

--- tcp/server/include/response.hpp
+++ tcp/server/include/response.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <cstdint>
+
 struct dctp_response_header {
     uint8_t return_code;
+    uint8_t operation_type;
     uint32_t id;
     int64_t result;
 };
@@ -12,4 +15,9 @@ enum return_code {
     DIV_BY_ZERO,
     FACT_OF_NEGATIVE,
     SQRT_OF_NEGATIVE
+};
+
+enum operation_type {
+    FAST = 1,
+    SLOW
 };

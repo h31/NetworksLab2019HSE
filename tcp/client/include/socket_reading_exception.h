f53f85b011e2526ffbfb83540c1f02b258572d3c
Dmitry <qudeed@gmail.com>
2019-02-18 00:04:56 +0300

    Clinet init commit

---
+++ tcp/client/include/socket_reading_exception.h
@@ -0,0 +1,9 @@
+#ifndef SERVER_SOCKET_IO_EXCEPTION_H
+#define SERVER_SOCKET_IO_EXCEPTION_H
+
+#include <exception>
+
+
+class socket_reading_exception : public std::exception {};
+
+#endif //SERVER_SOCKET_IO_EXCEPTION_H

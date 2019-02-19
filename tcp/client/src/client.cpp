87dd3eeaf9e408960034dffc2b8595c127a358f2
Александра Орлова <aleksandra.orlova@aleksandraorlova-nb.local>
2019-02-19 21:44:46 +0300

    littel fixes

--- tcp/client/src/client.cpp
+++ tcp/client/src/client.cpp
@@ -31,31 +31,26 @@ client::client(const char *host, uint16_t port_number) {
 }
 
 void client::registerUser() {
-    std::string pos;
-    int position = 0;
-    std::cin >> pos;
-    if (pos == "tester") {
-        position = 2;
-    }
-    if (pos == "developer") {
-        position = 1;
-    }
-    if (position == 0) {
-        std::cout << "Undefined position: " + pos + '\n';
-        return;
-    }
+    int usr_id;
+    std::cin >> usr_id;
     sendNum(REGISTER_USER);
-    sendNum(position);
+    sendNum(usr_id);
     int answer_type = getNum();
     switch (answer_type) {
         case REGISTER_USER_SUCC: {
-            user_id = getNum();
-            std::cout << "Register user with id: " + std::to_string(user_id) + '\n';
+            int role = getNum();
+            std::cout << "Register user with id: " + std::to_string(role) + '\n';
             break;
         }
-        case REGISTER_USER_UNDEFINED_TYPE: {
+        case REGISTER_USER_UNDEFINED_ID: {
             int type = getNum();
-            std::cout << "Can't register user with type code " + std::to_string(type) + '\n';
+            std::cout << "Can't register user id " + std::to_string(type) + '\n';
+            break;
+        }
+        case REGISTER_USER_AUTORIZE: {
+            int user_id = getNum();
+            int user_role = getNum();
+            std::cout << "User with id " << user_id << " and role " << user_role << " already authorized\n";
             break;
         }
         default: {
@@ -118,16 +113,33 @@ void client::getBugsTester() {
     sendNum(GET_TESTER_BUG_LIST);
     sendNum(bug_code);
     int type = getNum();
-    if (type != GET_TESTER_BUG_LIST_SUCC) {
-        std::cout << "Undefined server code\n";
-        return;
-    }
-    int bug_count = getNum();
-    for (int i = 0; i < bug_count; ++i) {
-        int bug_id = getNum();
-        int project_id = getNum();
-        std::string description = getString();
-        std::cout << "Bug with id: " << bug_id << "project id: " << project_id << " description: " << description;
+    switch (type) {
+        case GET_TESTER_BUG_LIST_SUCC: {
+            int bug_count = getNum();
+            for (int i = 0; i < bug_count; ++i) {
+                int bug_id = getNum();
+                int project_id = getNum();
+                std::string description = getString();
+                std::cout << "Bug with id: " << bug_id << "project id: " << project_id << " description: " << description;
+            }
+            break;
+        }
+        case UNKNOWN_REQUEST: {
+            int ver_code = getNum();
+            std::cout << "Unknown request code " << ver_code << '\n';
+            break;
+        }
+        case UNAUTORIZE: {
+            std::cout << "You not authorize\n";
+            break;
+        }
+        case CHECK_RIGHTS: {
+            std::cout << "You don't have rights\n";
+            break;
+        }
+        default: {
+            std::cout << "Undefined server code\n";
+        }
     }
 }
 
@@ -152,6 +164,29 @@ void client::confirmBugTester() {
         case PROCESS_BUG_TESTER_NO_BUG_ID: {
             bug_id = getNum();
             std::cout << "No bug with id: " << bug_id << '\n';
+            break;
+        }
+        case PROCESS_BUG_TESTER_NOT_FIX: {
+            bug_id = getNum();
+            std::cout << "Bug with id: " << bug_id << ' not fix' << '\n';
+            break;
+        }
+        case PROCESS_BUG_TESTER_UNKNOWN_CODE: {
+            int ver_code = getNum();
+            std::cout << "Unknown verify code " << ver_code << '\n';
+            break;
+        }
+        case UNKNOWN_REQUEST: {
+            int ver_code = getNum();
+            std::cout << "Unknown request code " << ver_code << '\n';
+            break;
+        }
+        case UNAUTORIZE: {
+            std::cout << "You not authorize\n";
+            break;
+        }
+        case CHECK_RIGHTS: {
+            std::cout << "You don't have rights\n";
             break;
         }
         default: {
@@ -183,6 +218,29 @@ void client::rejectBugTester() {
             std::cout << "No bug with id: " << bug_id << '\n';
             break;
         }
+        case PROCESS_BUG_TESTER_NOT_FIX: {
+            bug_id = getNum();
+            std::cout << "Bug with id: " << bug_id << ' not fix' << '\n';
+            break;
+        }
+        case PROCESS_BUG_TESTER_UNKNOWN_CODE: {
+            int ver_code = getNum();
+            std::cout << "Unknown verify code " << ver_code << '\n';
+            break;
+        }
+        case UNKNOWN_REQUEST: {
+            int ver_code = getNum();
+            std::cout << "Unknown request code " << ver_code << '\n';
+            break;
+        }
+        case UNAUTORIZE: {
+            std::cout << "You not authorize\n";
+            break;
+        }
+        case CHECK_RIGHTS: {
+            std::cout << "You don't have rights\n";
+            break;
+        }
         default: {
             std::cout << "Undefined server code\n";
         }
@@ -191,7 +249,6 @@ void client::rejectBugTester() {
 
 void client::getBugsDeveloper() {
     sendNum(GET_DEVELOPER_BUGS);
-    sendNum(user_id);
     int type = getNum();
     switch (type) {
         case GET_DEVELOPER_BUGS_SUCC: {
@@ -208,6 +265,19 @@ void client::getBugsDeveloper() {
             std::cout << "No developer with id: " << developer_id << '\n';
             break;
         }
+        case UNKNOWN_REQUEST: {
+            int ver_code = getNum();
+            std::cout << "Unknown request code " << ver_code << '\n';
+            break;
+        }
+        case UNAUTORIZE: {
+            std::cout << "You not authorize\n";
+            break;
+        }
+        case CHECK_RIGHTS: {
+            std::cout << "You don't have rights\n";
+            break;
+        }
         default: {
             std::cout << "Undefined server code\n";
             break;
@@ -218,7 +288,7 @@ void client::getBugsDeveloper() {
 void client::closeBugDeveloper() {
     int bud_id;
     std::cin >> bud_id;
-    sendNum(6686);
+    sendNum(CLOSE_BUG_DEVELOPER);
     sendNum(bud_id);
     int type = getNum();
     switch (type) {
@@ -232,6 +302,24 @@ void client::closeBugDeveloper() {
             std::cout << "No bug with id: " << bud_id << '\n';
             break;
         }
+        case CLOSE_BUG_DEVELOPER_ALREADY_CLOSE: {
+            int bug_status = getNum();
+            std::cout << "Bug already close : " << bug_status << '\n';
+            break;
+        }
+        case UNKNOWN_REQUEST: {
+            int ver_code = getNum();
+            std::cout << "Unknown request code " << ver_code << '\n';
+            break;
+        }
+        case UNAUTORIZE: {
+            std::cout << "You not authorize\n";
+            break;
+        }
+        case CHECK_RIGHTS: {
+            std::cout << "You don't have rights\n";
+            break;
+        }
         default: {
             std::cout << "Undefined server code\n";
             break;
@@ -240,19 +328,40 @@ void client::closeBugDeveloper() {
 }
 
 void client::addNewBug() {
-    int developer_id = 0, project_id = 0;
+    int developer_id = 0, project_id = 0, bug_id = 0;
     std::string description;
-    std::cout << developer_id << project_id << description;
+    std::cout << bug_id <<  developer_id << project_id << description;
     sendNum(SEND_NEW_BUG);
+    sendNum(bug_id);
     sendNum(developer_id);
     sendNum(project_id);
     sendString(description);
     int type = getNum();
-    if (type == SEND_NEW_BUG_SUCC) {
-        int bug_id = getNum();
-        std::cout << "Create new bug with id: " << bug_id << '\n';
-    } else {
-        std::cout << "Undefined server code\n";
+    switch (type) {
+        case SEND_NEW_BUG_SUCC: {
+            int bug_id = getNum();
+            std::cout << "Create new bug with id: " << bug_id << '\n';
+            break;
+        }
+        case SEND_NEW_BUG_EXIST: {
+            int bug_id = getNum();
+            std::cout << "Bud with id: " << bug_id << "exist \n";
+            break;
+        }
+        case UNKNOWN_REQUEST: {
+            int ver_code = getNum();
+            std::cout << "Unknown request code " << ver_code << '\n';
+            break;
+        }
+        case UNAUTORIZE: {
+            std::cout << "You not authorize\n";
+            break;
+        }
+        case CHECK_RIGHTS: {
+            std::cout << "You don't have rights\n";
+            break;
+        }
+        default:break;
     }
 }
 
--- tcp/client/src/client.h
+++ tcp/client/src/client.h
@@ -7,28 +7,35 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-enum position {TESTER = 2, DEVELOPER = 1};
 enum bug_status {ACTIVE = 0, CLOSE = 1, UNDEFINED = 3};
 enum verification_code {DECLINE = 0, ACCEPT = 1};
 enum message_code {
     REGISTER_USER = 100,
     REGISTER_USER_SUCC = 101,
-    REGISTER_USER_UNDEFINED_TYPE = 102,
+    REGISTER_USER_UNDEFINED_ID = 102,
+    REGISTER_USER_AUTORIZE = 103,
     GET_TESTER_BUG_LIST = 200,
     GET_TESTER_BUG_LIST_SUCC = 201,
     PROCESS_BUG_TESTER = 300,
     PROCESS_BUG_TESTER_SUCC = 301,
     PROCESS_BUG_TESTER_NON_ACTIVE_BUG = 302,
     PROCESS_BUG_TESTER_NO_BUG_ID = 303,
+    PROCESS_BUG_TESTER_NOT_FIX = 304,
+    PROCESS_BUG_TESTER_UNKNOWN_CODE = 305,
     GET_DEVELOPER_BUGS = 400,
     GET_DEVELOPER_BUGS_SUCC = 401,
     GET_DEVELOPER_BUGS_NO_ID = 402,
     CLOSE_BUG_DEVELOPER = 500,
     CLOSE_BUG_DEVELOPER_SUCC = 501,
     CLOSE_BUG_DEVELOPER_NO_ID = 502,
+    CLOSE_BUG_DEVELOPER_ALREADY_CLOSE = 503,
     SEND_NEW_BUG = 600,
     SEND_NEW_BUG_SUCC = 601,
-    CLIENT_TERMINATE = 700
+    SEND_NEW_BUG_EXIST = 602,
+    CLIENT_TERMINATE = 700,
+    UNKNOWN_REQUEST = 1,
+    UNAUTORIZE = 2,
+    CHECK_RIGHTS = 3,
 };
 
 class client {
@@ -44,7 +51,6 @@ class client {
         void quit();
 
     private:
-        int user_id;
         int sockfd;
         int getNum();
         void sendNum(int num);

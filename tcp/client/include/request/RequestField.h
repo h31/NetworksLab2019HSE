dd84ad12ef5aded023e51aaac6b0b59ad31b5039
vasyoid <vasyoid@gmail.com>
2019-02-19 22:03:19 +0300

    Refactoring and code cleanup

--- tcp/client/include/request/RequestField.h
+++ tcp/client/include/request/RequestField.h
@@ -1,5 +1,3 @@
-#include <utility>
-
 #ifndef CLIENT_REQUEST_FIELD_H
 #define CLIENT_REQUEST_FIELD_H
 
@@ -22,96 +20,23 @@ private:
     uint32_t int_value;
     std::string string_value;
 
-    bool writeInt(int socketfd) {
-        uint32_t nval = htonl(int_value);
-        return ::write(socketfd, &nval, sizeof(nval)) == sizeof(nval);
-    }
-
-    bool readInt(int socketfd) {
-        uint32_t nval;
-        bool result = ::read(socketfd, &nval, sizeof(nval)) == sizeof(nval);
-        if (result) {
-            int_value = ntohl(nval);
-        }
-        return result;
-    }
-
-    bool writeByte(int socketfd) {
-        return ::write(socketfd, &byte_value, sizeof(byte_value)) == sizeof(byte_value);
-    }
-
-    bool readByte(int socketfd) {
-        return ::read(socketfd, &byte_value, sizeof(byte_value)) == sizeof(byte_value);
-    }
-
-    bool writeString(int socketfd) {
-        return ::write(socketfd, string_value.c_str(), string_value.size() + 1) == string_value.size() + 1;
-    }
-
-    bool readString(int socketfd) {
-        string_value = std::string();
-        uint8_t next_char;
-        do {
-            if (::read(socketfd, &next_char, sizeof(next_char)) < sizeof(next_char)) {
-                return false;
-            }
-            if (next_char == 0) {
-                break;
-            }
-            string_value += next_char;
-        } while (next_char != 0);
-        return true;
-    }
+    bool writeInt(int socketfd);
+    bool readInt(int socketfd);
+    bool writeByte(int socketfd);
+    bool readByte(int socketfd);
+    bool writeString(int socketfd);
+    bool readString(int socketfd);
 
 public:
-
-    explicit RequestField(Type type): type(type) { }
-
-    explicit RequestField(uint8_t val): type(Type::BYTE) {
-        byte_value = val;
-    }
-
-    explicit RequestField(uint32_t val): type(Type::INT) {
-        int_value = val;
-    }
-
-    explicit RequestField(const std::string &val): type(Type::STRING) {
-        string_value = val;
-    }
-
-    bool write(int socketfd) {
-        switch (type) {
-            case Type::INT:
-                return writeInt(socketfd);
-            case Type::BYTE:
-                return writeByte(socketfd);
-            case Type::STRING:
-                return writeString(socketfd);
-        }
-    }
-
-    bool read(int socketfd) {
-        switch (type) {
-            case Type::INT:
-                return readInt(socketfd);
-            case Type::BYTE:
-                return readByte(socketfd);
-            case Type::STRING:
-                return readString(socketfd);
-        }
-    }
-
-    std::string getString() const {
-        return type == Type::STRING ? string_value : "";
-    }
-
-    uint32_t getInt() const {
-        return type == Type::INT ? int_value : 0;
-    }
-
-    uint8_t getByte() const {
-        return static_cast<uint8_t>(type == Type::BYTE ? byte_value : 0);
-    }
+    explicit RequestField(Type type);
+    explicit RequestField(uint8_t val);
+    explicit RequestField(uint32_t val);
+    explicit RequestField(const std::string &val);
+    bool write(int socketfd);
+    bool read(int socketfd);
+    std::string getString() const;
+    uint32_t getInt() const;
+    uint8_t getByte() const;
 };
 
 
--- tcp/client/src/request/RequestField.cpp
+++ tcp/client/src/request/RequestField.cpp
@@ -1,1 +1,90 @@
 #include "request/RequestField.h"
+
+bool RequestField::writeInt(int socketfd) {
+    uint32_t nval = htonl(int_value);
+    return ::write(socketfd, &nval, sizeof(nval)) == sizeof(nval);
+}
+
+bool RequestField::readInt(int socketfd) {
+    uint32_t nval;
+    bool result = ::read(socketfd, &nval, sizeof(nval)) == sizeof(nval);
+    if (result) {
+        int_value = ntohl(nval);
+    }
+    return result;
+}
+
+bool RequestField::writeByte(int socketfd) {
+    return ::write(socketfd, &byte_value, sizeof(byte_value)) == sizeof(byte_value);
+}
+
+bool RequestField::readByte(int socketfd) {
+    return ::read(socketfd, &byte_value, sizeof(byte_value)) == sizeof(byte_value);
+}
+
+bool RequestField::writeString(int socketfd) {
+    return ::write(socketfd, string_value.c_str(), string_value.size() + 1) == string_value.size() + 1;
+}
+
+bool RequestField::readString(int socketfd) {
+    string_value = std::string();
+    uint8_t next_char;
+    do {
+        if (::read(socketfd, &next_char, sizeof(next_char)) < sizeof(next_char)) {
+            return false;
+        }
+        if (next_char == 0) {
+            break;
+        }
+        string_value += next_char;
+    } while (next_char != 0);
+    return true;
+}
+
+RequestField::RequestField(RequestField::Type type) : type(type) { }
+
+RequestField::RequestField(uint8_t val) : type(Type::BYTE) {
+    byte_value = val;
+}
+
+RequestField::RequestField(uint32_t val) : type(Type::INT) {
+    int_value = val;
+}
+
+RequestField::RequestField(const std::string &val) : type(Type::STRING) {
+    string_value = val;
+}
+
+bool RequestField::write(int socketfd) {
+    switch (type) {
+        case Type::INT:
+            return writeInt(socketfd);
+        case Type::BYTE:
+            return writeByte(socketfd);
+        case Type::STRING:
+            return writeString(socketfd);
+    }
+}
+
+bool RequestField::read(int socketfd) {
+    switch (type) {
+        case Type::INT:
+            return readInt(socketfd);
+        case Type::BYTE:
+            return readByte(socketfd);
+        case Type::STRING:
+            return readString(socketfd);
+    }
+}
+
+std::string RequestField::getString() const {
+    return type == Type::STRING ? string_value : "";
+}
+
+uint32_t RequestField::getInt() const {
+    return type == Type::INT ? int_value : 0;
+}
+
+uint8_t RequestField::getByte() const {
+    return static_cast<uint8_t>(type == Type::BYTE ? byte_value : 0);
+}

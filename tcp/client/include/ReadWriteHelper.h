2a35343fb4d73b7c558c8f0e9f1fe06187c5826e
alyokhina-o <alyokhina-o@yandex-team.ru>
2019-02-20 19:19:02 +0300

    added getTopics

---
+++ tcp/client/include/ReadWriteHelper.h
@@ -0,0 +1,21 @@
+
+#ifndef CLIENT_READWRITEHELPER_H
+#define CLIENT_READWRITEHELPER_H
+
+#include <cstdint>
+#include <cstring>
+#include <string>
+
+class ReadWriteHelper {
+public:
+    static uint64_t get8Bytes(uint8_t* buffer, int &offset);
+    static uint32_t get4Bytes(uint8_t* buffer, int &offset);
+    static void set4Bytes(uint8_t* buffer, int &offset, uint32_t value);
+    static void set8Bytes(uint8_t* buffer, int &offset, uint64_t value);
+    static uint8_t getByte(uint8_t* buffer, int &offset);
+    static void setByte(uint8_t* buffer, int &offset, uint32_t value);
+    static std::string get32Byte(uint8_t* buffer, int &offset);
+    static void set32Byte(uint8_t* buffer, int &offset, std::string value);
+};
+
+#endif //CLIENT_READWRITEHELPER_H
--- tcp/client/src/Main.cpp
+++ tcp/client/src/Main.cpp
@@ -4,7 +4,20 @@
 #include <iostream>
 #include "../include/Client.h"
 int main(int argc, char *argv[]) {
+
     Client client;
-    client.start();
+    char* host;
+    uint16_t port;
+    if (argc < 3)  {
+        std::cout << "Введите хост: ";
+        std::cin >> host;
+        std::cout << "Введите порт: ";
+        std::cin >> port;
+    }
+    else {
+        host = argv[1];
+        port = (uint16_t) atoi(argv[2]);
+    }
+    client.start(host, port);
 }
 

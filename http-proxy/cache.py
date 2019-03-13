1bfee651c701d6d9fafaaf83001e6e087a861fb7
Александра Орлова <aleksandra.orlova@aleksandraorlova-nb.local>
2019-03-13 20:17:20 +0300

    improve cache

--- http-proxy/cache.py
+++ http-proxy/cache.py
@@ -1,14 +1,37 @@
-class Cache:
+import time
+import sys
 
-    def __init__(self):
+
+class Cache:
+    def __init__(self, expire, max_size):
         self.__cache = {}
+        self.__expire = expire
+        self.__max_size = max_size
 
     def get(self, request):
         if request.can_cache():
-            return self.__cache.update(request)
+            return self.__check_expire(request)
         else:
             return None
 
     def put(self, request, response):
-        if request.can_cache() and response.can_cache():
-            self.__cache[request] = response
+        if request.can_cache() and response.can_cache() and self.__clear_old_cache:
+            self.__cache[request] = (time.time(), response)
+
+    def __check_expire(self, request):
+        set_time, value = self.__cache.get(request)
+        if set_time + self.__expire > time.time():
+            self.__cache.pop(request)
+            return None
+        else:
+            return value
+
+    def __check_cache_size(self):
+        return sys.getsizeof(self.__cache) < self.__max_size
+
+    def __clear_old_cache(self):
+        if not self.__check_cache_size():
+            for key, set_time, value in dict.items():
+                if set_time + self.__expire > time.time():
+                    self.__cache.pop(key)
+        return self.__check_cache_size()

b94f9a45590e151fd6fd8d38cc052449aaf73513
Александра Орлова <aleksandra.orlova@aleksandraorlova-nb.local>
2019-03-13 17:48:47 +0300

    add message and simple cache

--- http-proxy/cache.py
+++ http-proxy/cache.py
@@ -1,10 +1,14 @@
 class Cache:
 
     def __init__(self):
-        pass
+        self.__cache = {}
 
-    def get(self, message):
-        pass
+    def get(self, request):
+        if request.can_cache():
+            return self.__cache.update(request)
+        else:
+            return None
 
-    def put(self, message):
-        pass
+    def put(self, request, response):
+        if request.can_cache() and response.can_cache():
+            self.__cache[request] = response
--- http-proxy/message.py
+++ http-proxy/message.py
@@ -1,27 +1,47 @@
+import re
+
 class Message:
 
     def __init__(self):
         self.__body = ""
         self.__start_line = ""
-        self.__headers = []
+        self.__headers = {}
 
     def set_start_line(self, start_line):
         self.__start_line = start_line
 
-    def add_header(self, header):
-        pass
+    def add_header(self, header_title, header_value):
+        self.__headers[header_title] = header_value
 
-    def set_body(self, body):
-        self.__body = body
+    def append_to_body(self, body):
+        self.__body += body
+        if len(self.__body) == self.__get_body_len():
+            return True
+        else:
+            return False
 
     def get_body(self):
         return self.__body
 
     def get_status(self):
-        pass
+        return self.__headers.get('Status:')
 
     def to_str(self):
-        pass
+        message = self.__start_line
+        for key, value in self.__headers.items():
+            message += key + ' ' + value
+        message += '\r\n'
+        message += self.__body
+        return message
 
     def get_host(self):
-        pass
+        if self.__headers.get('Host:') is not None:
+            return re.split(r":", self.__headers.get('Host:'))[0]
+        else:
+            return None
+
+    def can_cache(self):
+        return re.search(r"no-cache|no-store", self.__headers.get('Host:')) is None
+
+    def __get_body_len(self):
+        return int(self.__headers.get('Content-Length:'))

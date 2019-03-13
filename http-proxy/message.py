d92d419b4548635a2238b2f4b2377f1fbb8dc3a4
vasyoid <vasyoid@gmail.com>
2019-03-13 23:55:10 +0300

    Refactor message class

--- http-proxy/message.py
+++ http-proxy/message.py
@@ -1,11 +1,13 @@
 from re import split, search
 
+NEW_LINE = b"\r\n"
+
 
 class Message:
 
     def __init__(self):
-        self.__body = ""
-        self.__start_line = ""
+        self.__body = b""
+        self.__start_line = b""
         self.__headers = {}
 
     def set_start_line(self, start_line):
@@ -14,35 +16,33 @@ class Message:
     def add_header(self, header_title, header_value):
         self.__headers[header_title] = header_value
 
-    def append_to_body(self, body):
-        self.__body += body
-        if len(self.__body) == self.__get_body_len():
-            return True
-        else:
-            return False
+    def append_to_body(self, chunk):
+        self.__body += chunk
+        return len(self.__body) == self.__get_body_len()
 
     def get_body(self):
         return self.__body
 
     def get_status(self):
-        return self.__headers.get("Status:")
+        return self.__headers.get(b"Status:")
 
     def to_str(self):
-        message = self.__start_line
-        for key, value in self.__headers.items():
-            message += key + " " + value
-        message += "\r\n"
-        message += self.__body
+        message = self.__start_line + NEW_LINE
+        message += NEW_LINE.join([b"%s %s" % header for header in self.__headers.items()])
+        message += NEW_LINE * 2
+        if self.__get_body_len() > 0:
+            message += self.__body + NEW_LINE
         return message
 
     def get_host(self):
-        if self.__headers.get("Host:") is not None:
-            return split(r":", self.__headers.get("Host:"))[0]
+        host = self.__headers.get(b"Host:")
+        if host:
+            return split(r":", host)[0]
         else:
             return None
 
     def can_cache(self):
-        return search(r"no-cache|no-store", self.__headers.get("Host:")) is None
+        return search(r"no-cache|no-store", self.__headers.get(b"Cache-Control:")) is None
 
     def __get_body_len(self):
-        return int(self.__headers.get("Content-Length:"))
+        return int(self.__headers.get(b"Content-Length:", b"0"))

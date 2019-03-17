5f73a72eb0d0db82b21f5f18e26cdf3b38643fda
Dmitry <qudeed@gmail.com>
2019-03-18 01:14:49 +0300

    Removed extra files

--- tcp/untitled/include/no_value_exception.h
+++
@@ -1,9 +0,0 @@
-#ifndef UNTITLED_NO_VALUE_EXCEPTION_H
-#define UNTITLED_NO_VALUE_EXCEPTION_H
-
-#include <exception>
-
-
-class no_value_exception : public std::exception {};
-
-#endif //UNTITLED_NO_VALUE_EXCEPTION_H

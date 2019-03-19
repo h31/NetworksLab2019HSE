a1a6c76ef91a257d7ad83f4755f33feca0ace4f6
subject-name-here <jura.paradov@gmail.com>
2019-03-19 09:52:25 +0300

    fixed reading

--- tcp/server/include/util.hpp
+++ tcp/server/include/util.hpp
@@ -35,21 +35,25 @@ ssize_t socket_write_response(int socket_descriptor, struct dctp_response_header
 }
 
 ssize_t socket_read_request(int socket_descriptor, struct dctp_request_header& request) {
-    ssize_t c = read(socket_descriptor, &request, sizeof(request));
+    ssize_t read_bytes = 0;
+    ssize_t struct_size = sizeof(request);
+    while (read_bytes < struct_size) {
+        ssize_t received = read(socket_descriptor, &request + read_bytes, static_cast<size_t>(struct_size - read_bytes));
+        if (received < 0) {
+            if (errno == EINTR) {
+                continue;
+            } else {
+                log_error("error while reading request");
+                return -1;
+            }
+        } else if (received == 0) {
+            log_error("socket was closed.");
+            return -1;
+        }
+        read_bytes += received;
+    }
+
     log("Server: socket " + std::to_string(socket_descriptor) + " sent request: " + request_to_string(request));
 
-    if (c < 0) {
-        log_error("error while reading request");
-        return -1;
-    }
-    if (c == 0) {
-        log_error("socket has closed.");
-        return -1;
-    }
-    if (c > 0 && c != sizeof(request)) {
-        log_error("data was not read fully.");
-        return -1;
-    }
-
-    return c;
+    return read_bytes;
 }

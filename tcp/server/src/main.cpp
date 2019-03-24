5510715ebdab9997a7947166673f782507ee2fcc
vasyoid <vasyoid@gmail.com>
2019-03-24 17:31:38 +0300

    Add port parameter validation

--- tcp/server/src/main.cpp
+++ tcp/server/src/main.cpp
@@ -8,7 +8,11 @@ int main(int argc, char **argv) {
         std::cerr << "Usage: server <PORT>" << std::endl;
         return 0;
     }
-    Server server(static_cast<unsigned short>(strtol(argv[1], nullptr, 10)));
+    auto port = static_cast<unsigned short>(strtol(argv[1], nullptr, 10));
+    if (port < 1024) {
+        std::cerr << "Invalid port number: must be 1024-65535" << std::endl;
+    }
+    Server server(port);
     server.Run();
     return 0;
 }
